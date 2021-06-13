336802c05a787d92723c72a425c6a0a70dafd727
seungyeonkk <seungyeon9319@gmail.com>
2021-06-13 12:04:07 +0900

    fix SafeAreaView css

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,13 +1,14 @@
 import React, {useState} from "react";
 import styled from "styled-components/native";
 import SearchInput from '../components/SearchInput';
-import {TouchableWithoutFeedback, Keyboard} from "react-native";
+import {TouchableWithoutFeedback, Keyboard, Platform} from "react-native";
 import HomeHeader from "../components/home/HomeHeader";
 import Constants from 'expo-constants';
 
 const Container = styled.SafeAreaView`
     flex: 1;
     background-color: ${({ theme }) => theme.background};
+    paddingTop: ${Platform.OS === 'android' ? 25 : 0};
   `;
 
 const ViewContent = styled.View`
